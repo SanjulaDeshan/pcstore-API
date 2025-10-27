5385de4c00ae7b21450798e038558aff2945ccab
Sanjula Deshan <95172391+SanjulaDeshan@users.noreply.github.com>
2025-10-27 23:37:18 +0530

    creating-dbcontext-class

---
+++ pcstore.API/Data/PCStoreDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pcstore.API.Models.Domain;
+
+namespace pcstore.API.Data
+{
+	public class PCStoreDbContext: DbContext
+	{
+        public PCStoreDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemSpecification> ItemsSpecification { get; set; }
+    }
+}
